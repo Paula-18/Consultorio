a582aa29f09aa7f91dd4bd136f8504c39ef00586
alumno <alumno@E114>
2019-08-08 07:51:15 -0700

    a

---
+++ Consultorio/Consulta.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Consultorio
+{
+    class Consulta
+    {
+        //Atributos (variables)
+        DateTime fecha;
+        Paciente paciente;
+        string sintomas;
+        string diagnostico;
+        string tratamiento;
+    }
+}
